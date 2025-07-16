865df105939ee209dafbe2ab5d2bfbed7686bd42
DanielSamer <danielsamer04@gmail.com>
2025-07-16 20:48:09 +0300

    initialize project structure with Spring Boot and Maven setup

---
+++ src/main/java/com/mudson/The_bank_api_product/TheBankApiProductApplication.java
@@ -0,0 +1,13 @@
+package com.mudson.The_bank_api_product;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheBankApiProductApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheBankApiProductApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mudson/The_bank_api_product/TheBankApiProductApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mudson.The_bank_api_product;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheBankApiProductApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
